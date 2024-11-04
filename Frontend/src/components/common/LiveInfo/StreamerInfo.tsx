5ab1047cf53f7b353959b8f1f6b7479065a5e433
skdltn210 <rhdmstn202@gmail.com>
2024-11-04 18:11:09 +0900

    ✨ Feat: StreamerInfo 구현

---
+++ Frontend/src/components/common/LiveInfo/StreamerInfo.tsx
@@ -0,0 +1,8 @@
+export default function StreamerInfo() {
+  return (
+    <div className="w-full max-w-4xl p-3">
+      <div className="font-bold text-lg text-lico-orange-2">About StreamerName</div>
+      <div className="font-medium text-lico-gray-1">Welcome to my channel!</div>
+    </div>
+  );
+}
--- Frontend/src/pages/LivePage/index.tsx
+++ Frontend/src/pages/LivePage/index.tsx
@@ -1,11 +1,13 @@
-import VideoPlayer from '@/components/common/VideoPlayer';
-import LiveInfo from '@/components/common/LiveInfo';
+import VideoPlayer from '@components/common/VideoPlayer';
+import LiveInfo from '@components/common/LiveInfo';
+import StreamerInfo from '@components/common/LiveInfo/StreamerInfo';
 
 export default function LivePage() {
   return (
     <>
       <VideoPlayer streamUrl="" />
       <LiveInfo />
+      <StreamerInfo />
     </>
   );
 }
