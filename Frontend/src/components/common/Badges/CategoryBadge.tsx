c47bdb23986e71124ae3b09a92ad66661e9dedf0
skdltn210 <rhdmstn202@gmail.com>
2024-11-04 19:27:59 +0900

    💄 Style: CategoryBadge 볼드체로 변경

--- Frontend/src/components/common/Badges/CategoryBadge.tsx
+++ Frontend/src/components/common/Badges/CategoryBadge.tsx
@@ -4,7 +4,7 @@ interface CategoryBadgeProps {
 
 export default function CategoryBadge({ category }: CategoryBadgeProps) {
   return (
-    <div className="rounded-full bg-lico-orange-2 px-2.5 pb-[1px] pt-[3px] font-medium text-sm text-lico-gray-5 hover:bg-lico-orange-1">
+    <div className="rounded-full bg-lico-orange-2 px-2.5 pb-[1px] pt-[3px] font-bold text-sm text-lico-gray-5 hover:bg-lico-orange-1">
       {category}
     </div>
   );
