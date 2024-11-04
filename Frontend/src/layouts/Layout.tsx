9e2f23a997d2075067e8db9a8915b6829a6c8310
skdltn210 <rhdmstn202@gmail.com>
2024-11-04 11:06:52 +0900

    ✨ Feat: 레이아웃 조정
    페이지가 화면 전체를 차지하도록 하고 배경색을 넣었습니다.

--- Frontend/src/layouts/Layout.tsx
+++ Frontend/src/layouts/Layout.tsx
@@ -3,11 +3,11 @@ import Navbar from '@components/layout/Navbar';
 
 export default function Layout() {
   return (
-    <>
+    <div className="flex h-screen">
       <Navbar />
-      <main className="ml-56 p-12">
+      <main className="ml-60 flex-1 overflow-auto bg-lico-gray-5 p-12">
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
