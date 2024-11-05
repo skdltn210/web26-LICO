5eb2a6396a1cb7def8c386f873eac5db64e403a8
skdltn210 <rhdmstn202@gmail.com>
2024-11-05 17:19:48 +0900

    ♻️ Refactor: HomePage 정렬 버튼 삭제

--- Frontend/src/pages/HomePage/index.tsx
+++ Frontend/src/pages/HomePage/index.tsx
@@ -1,36 +1,9 @@
 import mockChannels from '@mocks/mockChannels';
 import ChannelGrid from '@/components/channel/ChannelGrid';
-import SortButton from '@components/common/Buttons/SortButton';
-import { useState } from 'react';
-import { FaFire } from 'react-icons/fa';
-import { LuClock, LuThumbsUp } from 'react-icons/lu';
-
-type SortType = 'popular' | 'recent' | 'recommended';
 
 function HomePage() {
-  const [activeSort, setActiveSort] = useState<SortType>('popular');
   return (
     <div className="p-12">
-      <div className="mb-3 flex gap-2 px-4">
-        <SortButton
-          label="인기"
-          isActive={activeSort === 'popular'}
-          icon={FaFire}
-          onClick={() => setActiveSort('popular')}
-        />
-        <SortButton
-          label="최신"
-          isActive={activeSort === 'recent'}
-          icon={LuClock}
-          onClick={() => setActiveSort('recent')}
-        />
-        <SortButton
-          label="추천"
-          isActive={activeSort === 'recommended'}
-          icon={LuThumbsUp}
-          onClick={() => setActiveSort('recommended')}
-        />
-      </div>
       <ChannelGrid channels={mockChannels} />
     </div>
   );
