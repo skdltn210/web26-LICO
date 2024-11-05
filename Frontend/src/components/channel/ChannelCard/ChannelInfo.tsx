5c484f3f8d247903d47f9125bf2cd9d6cd7dfe49
skdltn210 <rhdmstn202@gmail.com>
2024-11-05 11:05:56 +0900

    💄 Style: 컴포넌트 스타일 수정
    중앙 정렬, transition-colors, font 수정 등

--- Frontend/src/components/channel/ChannelCard/ChannelInfo.tsx
+++ Frontend/src/components/channel/ChannelCard/ChannelInfo.tsx
@@ -12,9 +12,9 @@ function ChannelInfo({ title, streamerName, category, profileImgUrl }: ChannelIn
     <div className="flex items-start gap-2 pt-2">
       <img className="h-10 w-10 cursor-pointer rounded-full bg-lico-gray-4" src={profileImgUrl} alt={streamerName} />
       <div className="cursor-pointer">
-        <h3 className="text-md line-clamp-2 max-h-[48px] font-bold text-lico-orange-2">{title}</h3>
-        <h3 className="line-clamp-1 text-xs text-lico-gray-2">{streamerName}</h3>
-        <Badge text={category} className="bg-lico-orange-2 text-lico-gray-1 hover:bg-lico-orange-1" />
+        <h3 className="line-clamp-2 max-h-[48px] font-bold text-base text-lico-orange-2">{title}</h3>
+        <h3 className="line-clamp-1 font-medium text-xs text-lico-gray-2">{streamerName}</h3>
+        <Badge text={category} className="bg-lico-orange-2 text-lico-gray-1 transition-colors hover:bg-lico-orange-1" />
       </div>
     </div>
   );
