f0c84450913e1ac665c3c9e24e79bfd9ed39ac35
pc5401 <pc5401@naver.com>
2024-11-05 17:14:20 +0900

    ✨ Feat: db-table 별 모듈 생성

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { StreamingModule } from './streaming/streaming.module';
+import { VideosModule } from './videos/videos.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule, CategoriesModule, StreamingModule, VideosModule],
   controllers: [AppController],
   providers: [AppService],
 })
