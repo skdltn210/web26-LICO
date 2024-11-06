ddc0c8b5ee80b07e634f97225df2db2c167b3f8d
gamgyul163 <gamgyul163@gmail.com>
2024-11-07 02:08:50 +0900

    ✨ feat: 라이브 목록 조회 api 추가

--- Backend/src/lives/lives.controller.ts
+++ Backend/src/lives/lives.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { LivesService } from './lives.service';
+import { LivesDto } from './dto/lives.dto';
 
 @Controller('lives')
-export class LivesController {}
+export class LivesController {
+  constructor(private readonly livesService: LivesService) {}
+
+  @Get()
+  async getLives(): Promise<LivesDto[]> {
+    return await this.livesService.readLives();
+  }
+}
--- Backend/src/lives/lives.service.ts
+++ Backend/src/lives/lives.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { LiveEntity } from './entity/live.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { LivesDto } from './dto/lives.dto';
 
 @Injectable()
-export class LivesService {}
+export class LivesService {
+  constructor(@InjectRepository(LiveEntity) private livesRepository: Repository<LiveEntity>) {}
+
+  async readLives(): Promise<LivesDto[]> {
+    const lives = await this.livesRepository.find({ relations: ['category'] });
+    return lives.map(entity => entity.toLivesDto());
+  }
+}
