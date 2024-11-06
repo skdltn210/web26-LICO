eeb946f5ef7cb3dd85608482cdec90fd7d97aef2
chologmaesil <parkseok9797@gmail.com>
2024-11-07 06:46:35 +0900

    ✨ Feat: Navbar 토글, 접기 구현
    - 화면 크기에 따른 상태 관리
    - Navbar 토글 기능
    - 축소 버전을 위한 NavbarMini 컴포넌트 구현

--- Frontend/src/components/layout/NavbarMini.tsx
+++ Frontend/src/components/layout/NavbarMini.tsx
@@ -1,50 +1,43 @@
 import { NavLink } from 'react-router-dom';
 import { LuMonitor, LuLayoutGrid, LuHeart, LuVideo, LuMenu } from 'react-icons/lu';
-import useLayoutStore from '@store/useLayoutStore';
+import useLayoutStore from '@store/useLayoutStore.ts';
 
 interface NavLinkProps {
   isActive: boolean;
 }
 
-export default function Navbar(): JSX.Element {
+export default function NavbarMini(): JSX.Element {
   const { toggleNavbar } = useLayoutStore();
+
   const linkClass = ({ isActive }: NavLinkProps): string =>
-    `flex items-center rounded-lg px-4 py-3 transition-colors hover:bg-lico-gray-3 hover:text-lico-orange-2 ${
+    `flex justify-center items-center rounded-lg px-4 py-3 transition-colors hover:bg-lico-gray-3 hover:text-lico-orange-2 ${
       isActive ? 'text-lico-orange-2' : ''
     }`;
 
   return (
-    <nav className="fixed left-0 top-0 z-10 h-screen w-60 border-r border-lico-gray-3 bg-lico-gray-4">
+    <nav className="fixed left-0 top-0 z-10 h-screen border-r border-lico-gray-3 bg-lico-gray-4">
       <div className="flex flex-col px-3 py-5">
-        <div className="mb-4 flex">
-          <button
-            type="button"
-            onClick={toggleNavbar}
-            className="justify-centerfont-bold flex items-center px-4 pb-4 text-3xl text-lico-orange-2"
-          >
-            <LuMenu size={36} />
-          </button>
-          <NavLink to="/" className="font-bold text-3xl text-lico-orange-2">
-            LICO
-          </NavLink>
-        </div>
+        <button
+          type="button"
+          onClick={toggleNavbar}
+          className="justify-centerfont-bold flex items-center px-4 pb-4 text-3xl text-lico-orange-2"
+        >
+          <LuMenu size={36} />
+        </button>
         <div className="flex flex-col text-lico-gray-1">
           <NavLink to="/lives" className={linkClass}>
             <div className="flex items-center">
               <LuMonitor className="-mt-0.5 h-5 w-5" />
-              <span className="ml-4 font-bold text-base">전체 방송</span>
             </div>
           </NavLink>
           <NavLink to="/category" className={linkClass}>
             <div className="flex items-center">
               <LuLayoutGrid className="-mt-0.5 h-5 w-5" />
-              <span className="ml-4 font-bold text-base">카테고리</span>
             </div>
           </NavLink>
           <NavLink to="/following" className={linkClass}>
             <div className="flex items-center">
               <LuHeart className="-mt-0.5 h-5 w-5" />
-              <span className="ml-4 font-bold text-base">팔로잉</span>
             </div>
           </NavLink>
 
@@ -53,7 +46,6 @@ export default function Navbar(): JSX.Element {
           <NavLink to="/studio" className={linkClass}>
             <div className="flex items-center">
               <LuVideo className="-mt-0.5 h-5 w-5" />
-              <span className="ml-4 font-bold text-base">스튜디오</span>
             </div>
           </NavLink>
         </div>
