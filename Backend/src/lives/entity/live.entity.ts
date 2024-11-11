08f8205f16e080d4bde842a337c52d87cb753b25
gamgyul163 <gamgyul163@gmail.com>
2024-11-11 14:05:47 +0900

    🐛 fix: livedto 변환 시 카테고리 정보 null 허용

--- Backend/src/lives/entity/live.entity.ts
+++ Backend/src/lives/entity/live.entity.ts
@@ -13,7 +13,6 @@ import {
 } from 'typeorm';
 import { LivesDto } from '../dto/lives.dto';
 import { LiveDto } from '../dto/live.dto';
-import { UpdateLiveDto } from '../dto/update.live.dto';
 
 @Entity('lives')
 export class LiveEntity {
@@ -59,8 +58,8 @@ export class LiveEntity {
 
   toLivesDto(): LivesDto {
     return {
-      categoriesId: this.category.id,
-      categoriesName: this.category.name,
+      categoriesId: this.category?.id,
+      categoriesName: this.category?.name,
       livesName: this.name,
       channelId: this.channelId,
       usersNickname: this.user.nickname,
@@ -70,21 +69,13 @@ export class LiveEntity {
 
   toLiveDto(): LiveDto {
     return {
-      categoriesId: this.category.id,
-      categoriesName: this.category.name,
+      categoriesId: this.category?.id,
+      categoriesName: this.category?.name,
       livesName: this.name,
       livesDescription: this.description,
       startedAt: this.startedAt,
       usersNickname: this.user.nickname,
       usersProfileImage: this.user.profileImage,
-    };
-  }
-
-  toUpdateLiveDto(): UpdateLiveDto {
-    return {
-      categoriesId: this.category.id,
-      name: this.name,
-      description: this.description,
     };
   }
 }
