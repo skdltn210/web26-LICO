72183ade2217b69778432035b7c395d3d72e0e6e
chologmaesil <parkseok9797@gmail.com>
2024-11-12 07:13:50 +0900

    ✨ Feat: Liveinfo 방송 시간 추가
    - StreamingTimer 컴포넌트를 구현했습니다.
    - Liveinfo 컴포넌트에 방송 시간을 추가했습니다.

--- Frontend/src/components/LiveInfo/index.tsx
+++ Frontend/src/components/LiveInfo/index.tsx
@@ -4,6 +4,7 @@ import FollowButton from '@components/common/Buttons/FollowButton';
 import { useState } from 'react';
 import { useChannel } from '@contexts/ChannelContext';
 import { formatNumber } from '@utils/format';
+import StreamingTimer from '@components/LiveInfo/StreamingTimer';
 
 interface LiveInfoProps {
   channelId: string;
@@ -19,12 +20,12 @@ export default function LiveInfo({ channelId }: LiveInfoProps) {
     <div className="w-full bg-lico-gray-4 p-3">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-3">
-          <div className="rounded-full bg-white p-2">
+          <div className="rounded-full bg-white">
             {!imageError ? (
               <img
                 src={currentChannel.profileImgUrl}
                 alt={currentChannel.streamerName}
-                className="h-6 w-6 rounded-full"
+                className="h-12 w-12 rounded-full"
                 onError={() => setImageError(true)}
               />
             ) : (
@@ -34,11 +35,12 @@ export default function LiveInfo({ channelId }: LiveInfoProps) {
           <div>
             <div className="font-bold text-base text-lico-orange-2">{currentChannel.streamerName}</div>
             <div className="font-medium text-sm text-lico-gray-2">{formatNumber(currentChannel.viewers)}명 시청중</div>
+            <StreamingTimer startAt={currentChannel.createdAt} />
           </div>
         </div>
         <FollowButton channelId={channelId} />
       </div>
-      <div className="mt-2 font-medium text-lico-gray-1">{currentChannel.title}</div>
+      <div className="mt-2 font-medium text-xl text-lico-gray-1">{currentChannel.title}</div>
       <div className="mt-2 flex items-center gap-2">
         <CategoryBadge
           category={currentChannel.category.name}
