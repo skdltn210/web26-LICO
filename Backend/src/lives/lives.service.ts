5d7c9fe3c59d01a5201ddef8f68688518087c72f
gamgyul163 <gamgyul163@gmail.com>
2024-11-11 14:24:33 +0900

    🐛 fix: 라이브 목록 조회 시 현재 방송중이 아닌 방송들도 같이 조회되는 문제 수정

--- Backend/src/lives/lives.service.ts
+++ Backend/src/lives/lives.service.ts
@@ -10,12 +10,11 @@ export class LivesService {
   constructor(@InjectRepository(LiveEntity) private livesRepository: Repository<LiveEntity>) {}
 
   async readLives(): Promise<LivesDto[]> {
-    const lives = await this.livesRepository.find({ relations: ['category', 'user'] });
+    const lives = await this.livesRepository.find({ relations: ['category', 'user'], where: { onAir: true } });
     return lives.map(entity => entity.toLivesDto());
   }
 
   async readLive(channelId: string): Promise<LiveDto> {
-    // TODO 라이브 테이블에 채널아이디로 인덱스 추가
     const live = await this.livesRepository.findOne({ relations: ['category', 'user'], where: { channelId } });
     return live.toLiveDto();
   }
