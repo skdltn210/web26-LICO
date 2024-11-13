28ca2c12d0999d0a2e262a64c0f894f7b720daf1
gamgyul163 <gamgyul163@gmail.com>
2024-11-13 22:45:11 +0900

    ✨ feat: 채팅방 신규 참여 시 기존 채팅 메시지 전송 추가

--- Backend/src/chats/chats.gateway.ts
+++ Backend/src/chats/chats.gateway.ts
@@ -1,12 +1,19 @@
 import { Inject } from '@nestjs/common';
-import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import Redis from 'ioredis';
 import { Server, Socket } from 'socket.io';
 
 const NAMESPACE_REGEX = /^\/chats\/[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 @WebSocketGateway({ namespace: NAMESPACE_REGEX })
-export class ChatsGateway {
+export class ChatsGateway implements OnGatewayConnection {
   @WebSocketServer()
   server: Server;
 
@@ -19,10 +26,16 @@ export class ChatsGateway {
     const parsedChat = JSON.parse(receivedChat);
     const chatToPublish = JSON.stringify({ message: parsedChat?.message }); // TODO 유저 정보 추가 필요
 
-    const redisKey = `${namespace.name}:chat`;
+    const redisKey = `${namespace.name}:chats`;
     this.redisClient.rpush(redisKey, chatToPublish);
     namespace.emit('chat', chatToPublish); // 해당 네임스페이스의 모든 클라이언트에게 메시지 전송
 
     this.redisClient.ltrim(redisKey, -50, -1); // TODO .env로 주입받기
+  }
+
+  async handleConnection(@ConnectedSocket() client: Socket) {
+    const oldChats = await this.redisClient.lrange(`${client.nsp.name}:chats`, 0, -1);
+
+    client.emit('oldChats', oldChats);
   }
 }
