c4bf3701f625c4ce2609632a87dfffe253338983
pc5401 <pc5401@naver.com>
2024-11-14 01:54:48 +0900

    ✨ Feat: users api 구현

--- Backend/src/users/users.service.ts
+++ Backend/src/users/users.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository, InjectDataSource } from '@nestjs/typeorm';
 import { UserEntity } from './entity/user.entity';
 import { LiveEntity } from '../lives/entity/live.entity';
 import { Repository, DataSource } from 'typeorm';
 import { CreateUserDto } from './dto/create.user.dto';
+import { UpdateUserDto } from './dto/update.user.dto';
 import { randomUUID } from 'crypto';
 
 @Injectable()
@@ -25,14 +26,14 @@ export class UsersService {
   ): Promise<UserEntity | null> {
     return this.usersRepository.findOne({
       where: { oauthUid, oauthPlatform },
-      relations: ['live'], // live 관계 추가
+      relations: ['live'],
     });
   }
 
   async findById(id: number): Promise<UserEntity | null> {
     return this.usersRepository.findOne({
       where: { id },
-      relations: ['live'], // live 관계 추가
+      relations: ['live'],
     });
   }
 
@@ -55,5 +56,23 @@ export class UsersService {
       });
       return await manager.save(UserEntity, newUser);
     });
+  }
+  // 사용자 프로필 업데이트 메서드 추가
+  async updateUser(userId: number, updateUserDto: UpdateUserDto): Promise<UserEntity> {
+    const user = await this.usersRepository.findOne({ where: { id: userId } });
+
+    if (!user) {
+      throw new NotFoundException('사용자를 찾을 수 없습니다.');
+    }
+
+    if (updateUserDto.nickname !== undefined) {
+      user.nickname = updateUserDto.nickname;
+    }
+
+    if (updateUserDto.profileImage !== undefined) {
+      user.profileImage = updateUserDto.profileImage;
+    }
+
+    return await this.usersRepository.save(user);
   }
 }
