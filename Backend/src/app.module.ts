b13861fc2d33b94f8af02e0f806136c7270674fc
gamgyul163 <gamgyul163@gmail.com>
2024-11-13 15:00:34 +0900

    ⚙️ config: typeorm DB_TYPE에 따라 사용되는 config 달라지게 변경

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -5,16 +5,18 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { CategoriesModule } from './categories/categories.module';
 import { VideosModule } from './videos/videos.module';
-
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { LivesModule } from './lives/lives.module';
+import sqliteConfig from './config/sqlite.config';
+import mysqlConfig from './config/mysql.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [mysqlConfig, sqliteConfig],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
