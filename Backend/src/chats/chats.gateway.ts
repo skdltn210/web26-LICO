1c6a6c896086533937fae71c58a0cd94167c006d
gamgyul163 <gamgyul163@gmail.com>
2024-11-14 16:56:22 +0900

    ✨ feat: 채팅 연결 시 인증 추가 및 채팅 메시지 폼 수정

--- Backend/src/chats/chats.gateway.ts
+++ Backend/src/chats/chats.gateway.ts
@@ -1,4 +1,5 @@
 import { InjectRedis } from '@nestjs-modules/ioredis';
+import { UseGuards } from '@nestjs/common';
 import {
   ConnectedSocket,
   MessageBody,
@@ -9,6 +10,9 @@ import {
 } from '@nestjs/websockets';
 import Redis from 'ioredis';
 import { Server, Socket } from 'socket.io';
+import { Handshake } from 'socket.io/dist/socket-types';
+import { WebSocketAuthGuard } from 'src/auth/guards/websocket-auth.guard';
+import { UserEntity } from 'src/users/entity/user.entity';
 
 const NAMESPACE_REGEX = /^\/chats\/[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
@@ -21,18 +25,29 @@ export class ChatsGateway implements OnGatewayConnection {
 
   @SubscribeMessage('chat')
   async publishChat(@ConnectedSocket() client: Socket, @MessageBody() receivedChat: string) {
-    const namespace = client.nsp;
-    // TODO 로그인 검증(JWT)
-    const parsedChat = JSON.parse(receivedChat);
+    const handShake = client.handshake as Handshake & { user: UserEntity };
+    const user = handShake.user;
 
-    const newChat = JSON.stringify({ message: parsedChat?.message }); // TODO 유저 정보 추가 필요
-    const redisKey = `${namespace.name}:chats`;
+    if (user.id === -1) {
+      client.emit('notify', JSON.stringify({ message: '채팅을 전송하려면 로그인 해야합니다.' }));
+    } else {
+      const namespace = client.nsp;
+      const parsedChat = JSON.parse(receivedChat);
+      const newChat = JSON.stringify({
+        content: parsedChat.message,
+        nickname: user.nickname,
+        userId: user.id,
+        timestamp: new Date(),
+      });
+      const redisKey = `${namespace.name}:chats`;
 
-    namespace.emit('chat', newChat); // 해당 네임스페이스의 모든 클라이언트에게 메시지 전송
-    this.redisClient.rpush(redisKey, newChat);
-    this.redisClient.ltrim(redisKey, -50, -1); // TODO .env로 주입받기
+      namespace.emit('chat', newChat); // 해당 네임스페이스의 모든 클라이언트에게 메시지 전송
+      this.redisClient.rpush(redisKey, newChat);
+      this.redisClient.ltrim(redisKey, -50, -1); // TODO .env로 주입받기
+    }
   }
 
+  @UseGuards(WebSocketAuthGuard)
   async handleConnection(@ConnectedSocket() client: Socket) {
     const oldChats = await this.redisClient.lrange(`${client.nsp.name}:chats`, 0, -1);
 
