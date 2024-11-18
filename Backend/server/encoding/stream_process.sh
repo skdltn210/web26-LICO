d75e11afb78aa8f117beafe08afe752c7b8b7e2c
gamgyul163 <gamgyul163@gmail.com>
2024-11-18 17:38:08 +0900

    ⚡️ perf: 인코딩 설정 변경

--- Backend/server/encoding/stream_process.sh
+++ Backend/server/encoding/stream_process.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-LOG_FILE="/lico/script/logfile.log"
-exec > >(tee -a "$LOG_FILE") 2>&1
+#LOG_FILE="/lico/script/logfile.log"
+#exec > >(tee -a "$LOG_FILE") 2>&1
 
 APP_NAME=$1
 STREAM_KEY=$2
@@ -23,11 +23,11 @@ if [[ -z "$CHANNEL_ID" ]]; then
     exit 1
 fi
 
-ffmpeg -rw_timeout 10000000 -i "rtmp://192.168.1.6:1935/$APP_NAME/$STREAM_KEY" -y\
-    -map 0:v -map 0:a -c:a aac -b:a 128k -c:v libx264 -b:v:2 2800k -s:v:2 1280x720 -preset veryfast -g 60 -tune zerolatency -profile:v baseline \
-    -map 0:v -map 0:a -c:a aac -b:a 128k -c:v libx264 -b:v:1 1200k -s:v:1 854x480 -preset veryfast -g 60 -tune zerolatency -profile:v baseline \
-    -map 0:v -map 0:a -c:a aac -b:a 128k -c:v libx264 -b:v:0 600k -s:v:0 640x360 -preset veryfast -g 60 -tune zerolatency -profile:v baseline \
-    -keyint_min 60  -hls_time 1 -hls_segment_type fmp4 -hls_flags delete_segments+independent_segments+append_list -hls_list_size 3 -hls_delete_threshold 5 \
+ffmpeg -rw_timeout 10000000 -r 30 -i "rtmp://192.168.1.6:1935/$APP_NAME/$STREAM_KEY" -y\
+    -map 0:v -map 0:a -c:a aac -b:a 128k -c:v libx264 -b:v:2 2800k -s:v:2 1280x720 -preset veryfast -g 30 -tune zerolatency -profile:v baseline \
+    -map 0:v -map 0:a -c:a aac -b:a 128k -c:v libx264 -b:v:1 1200k -s:v:1 854x480 -preset veryfast -g 30 -tune zerolatency -profile:v baseline \
+    -map 0:v -map 0:a -c:a aac -b:a 128k -c:v libx264 -b:v:0 600k -s:v:0 640x360 -preset veryfast -g 30 -tune zerolatency -profile:v baseline \
+    -keyint_min 30  -hls_time 1 -hls_segment_type fmp4 -hls_flags delete_segments+independent_segments+append_list -hls_list_size 3 -hls_delete_threshold 5 \
     -hls_segment_filename "/lico/storage/$APP_NAME/$CHANNEL_ID/%v/%03d.m4s" \
     -master_pl_name "index.m3u8" \
     -var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2" \
