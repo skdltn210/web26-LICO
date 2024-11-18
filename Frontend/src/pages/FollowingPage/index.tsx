2b3fd63a62603fd73dbdc11b9ff4129783b236a8
skdltn210 <rhdmstn202@gmail.com>
2024-11-19 00:28:50 +0900

    ✨ Feat: 오프라인 UI 추가

--- Frontend/src/pages/FollowingPage/index.tsx
+++ Frontend/src/pages/FollowingPage/index.tsx
@@ -1,11 +1,11 @@
 import ChannelGrid from '@components/channel/ChannelGrid';
+import OfflineGrid from './OfflineGrid';
 import { useFollow } from '@/hooks/useFollow';
-import type { ChannelCardProps } from '@components/channel/ChannelCard';
 
 export default function FollowingPage() {
   const { follows, isLoadingFollows } = useFollow();
 
-  const followedChannels: ChannelCardProps[] =
+  const followedChannels =
     follows?.map(follow => ({
       id: follow.channelId,
       title: follow.livesName,
@@ -19,12 +19,15 @@ export default function FollowingPage() {
       createdAt: new Date().toISOString(),
     })) ?? [];
 
+  const liveChannels = followedChannels.filter(channel => channel.isLive);
+  const offlineChannels = followedChannels.filter(channel => !channel.isLive);
+
   if (isLoadingFollows) {
     return (
       <div className="p-12">
         <div className="mb-3 px-4 font-bold text-2xl text-lico-gray-1">팔로잉</div>
         <div className="flex min-h-[300px] items-center justify-center">
-          <div className="text-lico-gray-2">로딩 중...</div>
+          <div className="font-bold text-lico-gray-2">로딩 중...</div>
         </div>
       </div>
     );
@@ -34,7 +37,7 @@ export default function FollowingPage() {
     return (
       <div className="p-12">
         <div className="mb-3 px-4 font-bold text-2xl text-lico-gray-1">팔로잉</div>
-        <div className="flex min-h-[300px] items-center justify-center font-medium text-lico-gray-2">
+        <div className="flex min-h-[300px] items-center justify-center font-bold text-lico-gray-2">
           아직 팔로우한 채널이 없습니다
         </div>
       </div>
@@ -43,8 +46,23 @@ export default function FollowingPage() {
 
   return (
     <div className="p-12">
-      <div className="mb-3 px-4 font-bold text-2xl text-lico-gray-1">팔로잉</div>
-      <ChannelGrid channels={followedChannels} />
+      <div className="mb-4 px-4 font-bold text-2xl text-lico-gray-1">팔로잉</div>
+
+      <div className="mb-4">
+        <div className="mb-4 flex items-center gap-2 px-4 font-bold text-xl">
+          <p className="text-lico-gray-1">라이브</p>
+          <span className="text-lico-orange-2">{liveChannels.length}</span>
+        </div>
+        <ChannelGrid channels={liveChannels} />
+      </div>
+
+      <div className="px-4">
+        <div className="mb-4 flex items-center gap-2 font-bold text-xl">
+          <p className="text-lico-gray-1">오프라인</p>
+          <span className="text-lico-orange-2">{offlineChannels.length}</span>
+        </div>
+        <OfflineGrid channels={offlineChannels} />
+      </div>
     </div>
   );
 }
