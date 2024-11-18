15922b6acb1e0451184dbae32b5edcea052e710b
gamgyul163 <gamgyul163@gmail.com>
2024-11-18 17:02:22 +0900

    ⚡️ perf: 인코딩 설정 변경

--- Backend/server/encoding/cleanup.sh
+++ Backend/server/encoding/cleanup.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
-LOG_FILE="/lico/script/cleanup.log"
-exec > >(tee -a "$LOG_FILE") 2>&1
+#LOG_FILE="/lico/script/cleanup.log"
+#exec > >(tee -a "$LOG_FILE") 2>&1
 
 
 APP_NAME=$1
-STREAM_KEY=$2
+CHANNEL_ID=$2
 
-sleep 30; # 송출 종료 30초 후 방종 처리
-
-echo "Cleanup FFmpeg script for APP_NAME: $APP_NAME, STREAM_KEY: $STREAM_KEY"
-# API 요청으로 채널 아이디 획득
-if [[ "$APP_NAME" == "live" ]]; then
-    CHANNEL_ID=$(curl -s http://192.168.1.9:3000/lives/channel-id/$STREAM_KEY | jq -r '.channelId')
-elif [[ "$APP_NAME" == "dev" ]]; then
-    CHANNEL_ID=$(curl -s http://192.168.1.7:3000/lives/channel-id/$STREAM_KEY | jq -r '.channelId')
-else
-    echo "Error: Unsupported APP_NAME. Exiting."
-    exit 1
-fi
-
+echo "Cleanup FFmpeg script for APP_NAME: $APP_NAME, CHANNEL_ID: $CHANNEL_ID"
 
 if [[ "$APP_NAME" == "live" ]]; then
     curl -X DELETE http://192.168.1.9:3000/lives/onair/$CHANNEL_ID
@@ -35,4 +23,3 @@ if [[ -d "/lico/storage/$APP_NAME/$CHANNEL_ID" ]]; then
     echo "Deleted directory: /lico/storage/$APP_NAME/$CHANNEL_ID"
 else
     echo "Directory /lico/storage/$APP_NAME/$CHANNEL_ID does not exist, skipping deletion."
-fi
