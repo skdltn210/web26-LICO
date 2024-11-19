40aa7dc357ced4856e7dccf6b6be48c5e06a5a18
skdltn210 <rhdmstn202@gmail.com>
2024-11-20 01:30:46 +0900

    🐛 Fix: CategoryBadge 중첩된 링크 에러 수정

--- Frontend/src/components/common/Badges/CategoryBadge.tsx
+++ Frontend/src/components/common/Badges/CategoryBadge.tsx
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom';
-
 interface CategoryBadgeProps {
   category: string;
   categoryId: number;
@@ -7,17 +5,15 @@ interface CategoryBadgeProps {
 }
 
 export default function CategoryBadge({ category, categoryId, className }: CategoryBadgeProps) {
-  const handleClick = (e: React.MouseEvent) => {
-    e.stopPropagation();
-  };
-
   return (
-    <Link to={`/category/${categoryId}`} onClick={handleClick}>
-      <div
-        className={`${className} inline-block cursor-pointer rounded-full bg-lico-orange-2 px-2.5 py-0.5 font-bold transition-colors hover:bg-lico-orange-1`}
-      >
-        {category}
-      </div>
-    </Link>
+    <div
+      onClick={e => {
+        e.stopPropagation();
+        window.location.href = `/category/${categoryId}`;
+      }}
+      className={`${className} inline-block cursor-pointer rounded-full bg-lico-orange-2 px-2.5 py-0.5 font-bold transition-colors hover:bg-lico-orange-1`}
+    >
+      {category}
+    </div>
   );
 }
