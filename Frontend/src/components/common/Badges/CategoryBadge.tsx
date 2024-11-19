d63d90bdb32cdf5e21c70a5595a3bb915e3aa470
skdltn210 <rhdmstn202@gmail.com>
2024-11-20 01:50:43 +0900

    🐛 Fix: ChannelCard의 CategoryBadge 클릭시 카테고리페이지로

--- Frontend/src/components/common/Badges/CategoryBadge.tsx
+++ Frontend/src/components/common/Badges/CategoryBadge.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 interface CategoryBadgeProps {
   category: string;
   categoryId: number;
@@ -6,14 +8,16 @@ interface CategoryBadgeProps {
 
 export default function CategoryBadge({ category, categoryId, className }: CategoryBadgeProps) {
   return (
-    <div
+    <Link
+      to={`/category/${categoryId}`}
       onClick={e => {
         e.stopPropagation();
+        e.preventDefault();
         window.location.href = `/category/${categoryId}`;
       }}
       className={`${className} inline-block cursor-pointer rounded-full bg-lico-orange-2 px-2.5 py-0.5 font-bold transition-colors hover:bg-lico-orange-1`}
     >
       {category}
-    </div>
+    </Link>
   );
 }
