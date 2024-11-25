7fc700b4cf31bac4d94d2efcf7f031eed7db7a9c
gamgyul163 <gamgyul163@gmail.com>
2024-11-25 20:06:48 +0900

    ⚙️ config: 방송 송출 종료 5초 후 방종 처리되도록 수정

--- Backend/server/encoding/stream_process.sh
+++ Backend/server/encoding/stream_process.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#LOG_FILE="/lico/script/logfile.log"
-#exec > >(tee -a "$LOG_FILE") 2>&1
+LOG_FILE="/lico/script/logfile.log"
+exec > >(tee -a "$LOG_FILE") 2>&1
 
 APP_NAME=$1
 STREAM_KEY=$2
@@ -17,25 +17,21 @@ else
     exit 1
 fi
 
-
 if [[ -z "$CHANNEL_ID" ]]; then
     echo "Error: CHANNEL_ID is empty. Exiting."
     exit 1
 fi
 
+
 ffmpeg -rw_timeout 10000000 -r 30 -i "rtmp://192.168.1.6:1935/$APP_NAME/$STREAM_KEY" \
         -filter_complex "[0:v]split=3[720p][for480p][for360p];[for480p]scale=854:480[480p];[for360p]scale=640:360[360p]" \
-        -map "[720p]" -map 0:a -c:v:0 libx264 -b:v:0 2800k -s:v:0 1280x720 -preset ultrafast -g 30 -tune zerolatency -profile:v:0 baseline -c:a:0 aac -b:a:0 128k \
-        -map "[480p]" -map 0:a -c:v:1 libx264 -b:v:1 1200k -preset ultrafast -g 30 -tune zerolatency -profile:v:1 baseline -c:a:1 copy \
-        -map "[360p]" -map 0:a -c:v:2 libx264 -b:v:2 600k -preset ultrafast -g 30 -tune zerolatency -profile:v:2 baseline -c:a:2 copy \
+        -map "[720p]" -map 0:a? -c:v:0 libx264 -b:v:0 2800k -s:v:0 1280x720 -preset ultrafast -g 30 -tune zerolatency -profile:v:0 baseline -c:a aac -b:a 128k \
+        -map "[480p]" -map 0:a? -c:v:1 libx264 -b:v:1 1200k -preset ultrafast -g 30 -tune zerolatency -profile:v:1 baseline -c:a aac -b:a 128k \
+        -map "[360p]" -map 0:a? -c:v:2 libx264 -b:v:2 600k -preset ultrafast -g 30 -tune zerolatency -profile:v:2 baseline -c:a aac -b:a 128k \
         -keyint_min 30 -hls_time 1 -hls_segment_type fmp4 -hls_flags delete_segments+independent_segments+append_list \
         -hls_list_size 3 -hls_delete_threshold 5 \
         -hls_segment_filename "/lico/storage/$APP_NAME/$CHANNEL_ID/%v/%03d.m4s" \
         -master_pl_name "index.m3u8" \
-        -var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2" \
+        -var_stream_map "v:2,a:2 v:1,a:1 v:0,a:0" \
         -f hls "/lico/storage/$APP_NAME/$CHANNEL_ID/%v/index.m3u8" \
         -map 0:v -vf "fps=1/10,scale=426:240" -update 1 "/lico/storage/$APP_NAME/$CHANNEL_ID/thumbnail.jpg"
-
-
-
-/lico/script/cleanup.sh "$APP_NAME" "$CHANNEL_ID"
