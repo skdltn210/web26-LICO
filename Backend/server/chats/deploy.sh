9e7085b98193d96f44aff327d426aa754cbd34ec
gamgyul163 <gamgyul163@gmail.com>
2024-11-27 21:01:03 +0900

    🐛 fix: 워크 플로 및 배포 스크립트 수정

--- Backend/server/chats/deploy.sh
+++ Backend/server/chats/deploy.sh
@@ -2,15 +2,25 @@
 set -e  # 에러 발생 시 즉시 스크립트 종료
 
 echo "배포 스크립트 시작"
+BRANCH_NAME=$1
+echo "배포 브랜치: $BRANCH_NAME"
 
-cd /lico/Backend
+if [ -z "$BRANCH_NAME" ]; then
+   echo "Error: Branch name is required."
+   exit 4
+fi
+
+cd /lico-chats/Backend
 
 # Git 작업
-git pull
+git fetch origin
+git checkout "$BRANCH_NAME"
+git reset --hard
+git pull origin "$BRANCH_NAME"
 
 # 의존성 설치 및 빌드
 npm install || exit 5
-npm run build chats || exit 6
+npm run build api || exit 6
 
 # 서버 재시작
 forever stop dist/apps/chats/main.js || true  # 기존 프로세스가 없어도 오류 발생 방지
