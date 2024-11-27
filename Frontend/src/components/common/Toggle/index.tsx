3838fa5f95acc9fd61ccb7a8cb3f5104fccc596f
chologmaesil <parkseok9797@gmail.com>
2024-11-27 17:38:08 +0900

    💄 Style: 스타일 오타 수정

--- Frontend/src/components/common/Toggle/index.tsx
+++ Frontend/src/components/common/Toggle/index.tsx
@@ -11,7 +11,7 @@ function Toggle({ checked, onChange, className = '' }: ToggleProps) {
       onClick={() => onChange(!checked)}
       className={`relative inline-flex h-6 w-14 items-center rounded-full transition-colors duration-200 ease-in-out ${checked ? 'bg-lico-orange-1' : 'bg-lico-gray-3'} ${className}`}
     >
-      <div className="justi absolute inset-0 flex items-center font-medium text-xs">
+      <div className="absolute inset-0 flex items-center justify-between px-1.5 font-medium text-xs">
         <span
           className={`font-bold text-lico-gray-3 transition-opacity duration-200 ${checked ? 'opacity-100' : 'opacity-0'}`}
         >
