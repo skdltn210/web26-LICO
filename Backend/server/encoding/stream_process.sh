04f7e7a982ea9a18c597916c299ce1ffe6599cc9
gamgyul163 <gamgyul163@gmail.com>
2024-12-01 19:00:45 +0900

    🐛 fix: 인코딩 옵션 조정(웹 스튜디오 송출 불가 문제 수정, 저장 경로 rclone마운트 디렉토리 로 변경

--- Backend/server/encoding/stream_process.sh
+++ Backend/server/encoding/stream_process.sh
@@ -23,15 +23,16 @@ if [[ -z "$CHANNEL_ID" ]]; then
 fi
 
 
-ffmpeg -rw_timeout 10000000 -r 30 -i "rtmp://192.168.1.6:1935/$APP_NAME/$STREAM_KEY" -y \
+ffmpeg -rw_timeout 20000000 -r 30 -i "rtmp://192.168.1.6:1935/$APP_NAME/$STREAM_KEY" -y \
         -filter_complex "[0:v]split=3[720p][for480p][for360p];[for480p]scale=854:480[480p];[for360p]scale=640:360[360p]" \
-        -map "[720p]" -map 0:a? -c:v:0 libx264 -b:v:0 2800k -s:v:0 1280x720 -preset ultrafast -g 30 -tune zerolatency -profile:v:0 baseline -c:a aac -b:a 128k \
-        -map "[480p]" -map 0:a? -c:v:1 libx264 -b:v:1 1200k -preset ultrafast -g 30 -tune zerolatency -profile:v:1 baseline -c:a aac -b:a 128k \
-        -map "[360p]" -map 0:a? -c:v:2 libx264 -b:v:2 600k -preset ultrafast -g 30 -tune zerolatency -profile:v:2 baseline -c:a aac -b:a 128k \
-        -keyint_min 30 -hls_time 1 -hls_segment_type fmp4 -hls_flags delete_segments+independent_segments+append_list \
-        -hls_list_size 3 -hls_delete_threshold 5 \
-        -hls_segment_filename "/lico/storage/$APP_NAME/$CHANNEL_ID/%v/%03d.m4s" \
+        -map "[720p]" -map 0:a? -c:v:0 libx264 -b:v:0 2800k -s:v:0 1280x720 -preset ultrafast -g 90 -tune zerolatency -profile:v:0 baseline -c:a:0 aac -b:a:0 128k \
+        -map "[480p]" -map 0:a? -c:v:1 libx264 -b:v:1 1200k -preset ultrafast -g 90 -tune zerolatency -profile:v:1 baseline -c:a:1 copy \
+        -map "[360p]" -map 0:a? -c:v:2 libx264 -b:v:2 600k -preset ultrafast -g 90 -tune zerolatency -profile:v:2 baseline -c:a:2 copy \
+        -keyint_min 90 -hls_segment_type fmp4 -hls_flags independent_segments+append_list \
+        -hls_list_size 2 \
+        -hls_segment_filename "/lico/rclone/$APP_NAME/$CHANNEL_ID/%v/%03d.m4s" \
         -master_pl_name "index.m3u8" \
         -var_stream_map "v:2,a:2 v:1,a:1 v:0,a:0" \
-        -f hls "/lico/storage/$APP_NAME/$CHANNEL_ID/%v/index.m3u8" \
-        -map 0:v -vf "fps=1/10,scale=426:240" -update 1 "/lico/storage/$APP_NAME/$CHANNEL_ID/thumbnail.jpg"
+        -f hls "/lico/rclone/$APP_NAME/$CHANNEL_ID/%v/index.m3u8" \
+        -map 0:v -vf "fps=1/10,scale=426:240" -update 1 "/lico/rclone/$APP_NAME/$CHANNEL_ID/thumbnail.jpg"
+
