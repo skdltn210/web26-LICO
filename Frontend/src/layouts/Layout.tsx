edbee4d6fa3c5a6fd0d87eb18c7ab6989f253c92
chologmaesil <parkseok9797@gmail.com>
2024-12-02 21:15:44 +0900

    ✨ Feat: 사이드 바 개선
    - 전역 상태로 채팅창, 비디오 컴포넌트와 같이 관리하던 Navbar를 독립적으로 관리하게 변경했습니다.
    - NavbarMini와 Navbar를 통합했습니다. isExpanded 상태로 펼쳤다 접게 됩니다.
    - 사이드바 상태가 전환될 때 부드러운 애니메이션을 추가했습니다.
    - NavItem 컴포넌트를 분리했습니다.
    - 아이콘 위치 크기를 조절하여 사이드바 상태가 전환될 때, 안정적이게 변경하였습니다.
    - 축소 상태일 때, 호버 시 툴팁이 보이도록 수정했습니다.

--- Frontend/src/layouts/Layout.tsx
+++ Frontend/src/layouts/Layout.tsx
@@ -1,18 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '@components/layout/Navbar';
-import NavbarMini from '@components/layout/NavbarMini';
-import useLayoutStore from '@store/useLayoutStore';
+import { useState } from 'react';
 
 export default function Layout() {
-  const { navbarState } = useLayoutStore();
-  const isHidden = navbarState === 'hidden';
-  const isCollapsed = navbarState === 'collapsed';
+  const [isExpanded, setIsExpanded] = useState(true);
+  const handleNavbarToggle = () => setIsExpanded(!isExpanded);
 
   return (
     <div className="flex h-screen">
-      {isHidden ? '' : isCollapsed ? <NavbarMini /> : <Navbar />}
+      <Navbar isExpanded={isExpanded} onToggle={handleNavbarToggle} />
+
       <main
-        className={`${!isHidden && 'ml-60'} ${isCollapsed && 'ml-[76px]'} scrollbar-hider flex-1 overflow-auto bg-lico-gray-5`}
+        className={`${isExpanded ? 'ml-60' : 'ml-[76px]'} scrollbar-hider flex-1 overflow-auto bg-lico-gray-5 transition-all duration-300`}
       >
         <Outlet />
       </main>
